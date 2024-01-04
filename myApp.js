ef077bbbc52bb06221e3cc638150165ab9a723d8
matiasd11 <88749703+matiasd11@users.noreply.github.com>
2024-01-04 02:26:11 +0000

    Update 3 files

--- myApp.js
+++ myApp.js
@@ -1,5 +1,7 @@
 let express = require("express");
 let app = express();
 console.log("Hello world");
-app.use(express.static(__dirname + "/public"));
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/public/index.html");
+});
 module.exports = app;
